bc8fcadec78957fa3fce09a2ca771077e4530af6
Matt Puchyr <matt.puchyr@gmail.com>
2019-08-19 23:47:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,31 @@
 # Write your code here!
 def game_hash
-
+  game = {
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => [
+        {"Alan Anderson" => {
+          :number => 0,
+          :shoe => 16,
+          :points => 22,
+          :rebouns => 12,
+          :assists => 12,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 1
+          
+        },
+        "Reggie Evans" => {
+          
+        },
+          
+        }
+        ]
+    },
+    :away => {}
+    
+  }
 end
 
 
