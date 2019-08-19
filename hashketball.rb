3dbc555e4779703c615bbb118c57855cc79a4388
Matt Puchyr <matt.puchyr@gmail.com>
2019-08-19 23:56:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -14,9 +14,8 @@ def game_hash
           :steals => 3,
           :blocks => 1,
           :slam_dunks => 1
-          
         },
-        "Reggie Evans" => {
+         "Reggie Evans" => {
           :number => 30,
           :shoe => 14,
           :points => 12,
@@ -26,8 +25,37 @@ def game_hash
           :blocks => 12,
           :slam_dunks => 7
         },
-          
-        }
+          "Brook Lopez" => {
+          :number => 11,
+          :shoe => 17,
+          :points => 17,
+          :rebounds => 19,
+          :assists => 10,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 15
+          }
+        },
+          "Mason Plumlee" => {
+          :number => 1,
+          :shoe => 19,
+          :points => 26,
+          :rebounds => 11,
+          :assists => 6,
+          :steals => 3,
+          :blocks => 8,
+          :slam_dunks => 5
+          },
+          "Jason Terry" => {
+          :number => 31,
+          :shoe => 15,
+          :points => 19,
+          :rebounds => 2,
+          :assists => 2,
+          :steals => 4,
+          :blocks => 11,
+          :slam_dunks => 1
+          }
         ]
     },
     :away => {}
