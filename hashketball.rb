22db5001736a1c54991cfdd540a2e1e6ba125125
Matt Puchyr <matt.puchyr@gmail.com>
2019-08-20 00:35:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -83,34 +83,34 @@ def game_hash
           :slam_dunks => 10
         },
         "DeSagna Diop" => {
-          :number => ,
-          :shoe => ,
-          :points => ,
-          :rebounds => ,
-          :assists => ,
-          :steals => ,
-          :blocks => ,
-          :slam_dunks =>
+          :number => 2,
+          :shoe => 14,
+          :points => 24,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 4,
+          :blocks => 5,
+          :slam_dunks => 5
         },
         "Ben Gordon" => {
-          :number => ,
-          :shoe => ,
-          :points => ,
-          :rebounds => ,
-          :assists => ,
-          :steals => ,
-          :blocks => ,
-          :slam_dunks =>
+          :number => 8,
+          :shoe => 15,
+          :points => 33,
+          :rebounds => 3,
+          :assists => 2,
+          :steals => 1,
+          :blocks => 1,
+          :slam_dunks => 0
         },
         "Kemba Walker" => {
-          :number => ,
-          :shoe => ,
-          :points => ,
-          :rebounds => ,
-          :assists => ,
-          :steals => ,
-          :blocks => ,
-          :slam_dunks =>
+          :number => 33,
+          :shoe => 15,
+          :points => 6,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 7,
+          :blocks => 5,
+          :slam_dunks => 12
         }
       }
     }
